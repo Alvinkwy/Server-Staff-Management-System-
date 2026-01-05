91dae1cc85a4b85cf190c4d3f1b9584c1828d63f
Alvinkwy <114460550+Alvinkwy@users.noreply.github.com>
2026-01-06 03:09:47 +0800

    Delete SQLQueryBackup the Certificate and Private Key.sql

--- SQLQueryBackup the Certificate and Private Key.sql
+++
@@ -1,9 +0,0 @@
-USE master;
-GO
-
-BACKUP CERTIFICATE StaffMgmtCert 
-TO FILE = 'C:\TDE\StaffMgmtCert.cer'
-WITH PRIVATE KEY (
-     FILE = 'C:\TDE\StaffMgmtCert.pvk',
-     ENCRYPTION BY PASSWORD = 'Pa$$w0rd'
-);
